97154a7c00202699bf743407a5982b449412a35d
apb_axel <apb.axel@gmail.com>
2014-08-28 22:38:33 -0400

    Added additional devices to UKM
    LG Optimus G
    LG G Pad 8.3
    Nexus 7 2012

--- data/UKM/device/.device.sh
+++ data/UKM/device/.device.sh
@@ -4,14 +4,20 @@ UKM=/data/UKM;
 BB=$UKM/busybox;
 DEVICE=`getprop ro.product.device`;
 
+#Official
 if [ $DEVICE == "mako" ]; then
 	CONFIG="mako"; #Nexus 4
 elif [ $DEVICE == "hammerhead" ]; then
 	CONFIG="hammerhead"; #Nexus 5
 elif [ $DEVICE == "deb" -o $DEVICE == "flo" ]; then
-	CONFIG="deb"; #Nexus 7
+	CONFIG="deb"; #Nexus 7 2013
 elif [ $DEVICE == "bacon" -o $DEVICE == "A0001" ]; then
 	CONFIG="bacon"; #OnePlus One
+#Unnofficial
+elif [ $DEVICE = "gee"* -o $DEVICE == "v500" ]; then
+	CONFIG="mako"; #gee = LG Optimus G, v500= LG G Pad 8.3
+elif [ $DEVICE == "grouper" -o $DEVICE == "tilapia" ]; then
+	CONFIG="deb"; #Nexus 7 2012
 else
 	CONFIG=""; #Not defined
 fi;
