b1e91b07245ec0abdf854a1e87dbe9217455c815
apb_axel <apb.axel@gmail.com>
2014-09-10 23:52:25 -0400

    Support for new devices
    * LG G2
    * HTC M7
    * Sony XPeria Z
    * Sony Xperia ZL

--- data/UKM/device/.device.sh
+++ data/UKM/device/.device.sh
@@ -14,10 +14,16 @@ elif [ $DEVICE == "deb" -o $DEVICE == "flo" ]; then
 elif [ $DEVICE == "bacon" -o $DEVICE == "A0001" ]; then
 	CONFIG="bacon"; #OnePlus One
 #Unnofficial
-elif [[ $DEVICE = "gee"* ]] || [ $DEVICE = "v500" ]; then
-	CONFIG="mako"; #gee = LG Optimus G, v500= LG G Pad 8.3
+elif [[ $DEVICE = "gee"* ]] || [ $DEVICE == "v500" ]; then
+	CONFIG="mako"; #gee* = LG Optimus G, v500= LG G Pad 8.3
+elif [[ $DEVICE = "m7"* ]]; then
+	CONFIG="mako"; #m7* = HTC One M7
+elif [ $DEVICE == "odin" -o $DEVICE == "yuga" ] || [[ $DEVICE = "C650"? ]] || [[ $DEVICE = "C660"? ]]; then
+	CONFIG="mako"; #odin|C650x = Sony Xperia ZL, yuga|C660x = Sony Xperia Z
 elif [ $DEVICE == "grouper" -o $DEVICE == "tilapia" ]; then
 	CONFIG="deb"; #Nexus 7 2012
+elif [ $DEVICE == "g2" -o $DEVICE == "galbi" -o $DEVICE == "ls980" -o $DEVICE == "vs980" ] || [[ $DEVICE = "d80"? ]]; then
+	CONFIG="bacon"; #g2|galbi|ls980|vs980|d80x = LG G2
 else
 	CONFIG=""; #Not defined
 fi;
