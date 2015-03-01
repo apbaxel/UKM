7d4a0a6b3c7312149de91476ba168064731acada
apb_axel <apb.axel@gmail.com>
2015-03-01 01:06:37 -0400

    New Devices
    * Additional LG G3 variants
    * Samsung Galaxy S4
    * Samsung Galaxy S5

--- data/UKM/device/.device.sh
+++ data/UKM/device/.device.sh
@@ -23,13 +23,13 @@ case $DEVICE in
 	gee*|v500|*m7*|odin|C650?|yuga|C660?|ghost*|xt105?|xt1060|obake*|xt1080*)
 		CONFIG="mako";; #gee* = LG Optimus G, v500 = LG G Pad 8.3, *m7* = HTC One M7, odin|C650x = Sony Xperia ZL, yuga|C660x = Sony Xperia Z
 			#ghost*|xt105x|xt1060 = Moto X 2013, obake*|xt1080* = Motorola Droid Ultra
-	g2|galbi|ls980|vs980|d80?|g3|*m8*|honami|C6943|C690?|togari|C68??|shamu)
-		CONFIG="hammerhead";; #g2|galbi|ls980|vs980|d80x = LG G2, g3 = LG G3, *m8* = HTC One M8, honami|C6943|C690x = Sony Xperia Z1, 
-			#togari|C68xx = Sony Xperia Z Ultra, shamu = Nexus 6
+	g2|galbi|ls980|vs980|d80?|g3|vs985|d85?|*m8*|honami|C6943|C690?|togari|C68??|shamu|d2*|jflte*|klte*)
+		CONFIG="hammerhead";; #g2|galbi|ls980|vs980|d80x = LG G2, g3|ls990|vs985|d85x = LG G3, *m8* = HTC One M8, honami|C6943|C690x = Sony Xperia Z1, 
+			#togari|C68xx = Sony Xperia Z Ultra, shamu = Nexus 6, , d2* = Samsung Galaxy S3, jflte* = Samsung Galaxy S4, klte* = Samsung Galaxy S5
 	grouper|tilapia)
 		CONFIG="deb";; #Nexus 7 2012
-	falcon*|peregrine*|sirius|D65??|titan*|d2*)
-		CONFIG="bacon";; #falcon* = Moto G 2013, peregrine* = Moto G 4G 2013, sirius|D65xx = Sony Xperia Z2, titan* = Moto G 2014, d2* = Samsung Galaxy S3
+	falcon*|peregrine*|sirius|D65??|titan*)
+		CONFIG="bacon";; #falcon* = Moto G 2013, peregrine* = Moto G 4G 2013, sirius|D65xx = Sony Xperia Z2, titan* = Moto G 2014
 	*)
 		CONFIG="";; #Not defined
 esac;
